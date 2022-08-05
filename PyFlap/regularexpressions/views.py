96411123d9406fe38b0c84fe97bf7cfc30211b20
SeaSkyThe <marceloer2011@gmail.com>
2022-08-04 23:02:45 -0300

    Fixing FOOTER, Sidebar LINK HIGHLIGHT

--- PyFlap/regularexpressions/views.py
+++ PyFlap/regularexpressions/views.py
@@ -12,12 +12,17 @@ def regex_page(request):
         form = RegexForm(request.POST)
         context['regexform'] = form
         if(form.is_valid()):
-            if(verify_regex(form['regularexpression'].value(), form['teststring'].value())):
-                context['regex_validation'] = True
-            else:
-                context['regex_validation'] = False
-        
-        return render(request, 'regularexpressions/index.html', context=context)
+            try:
+                if(verify_regex(form['regularexpression'].value(), form['teststring'].value())):
+                    context['string_validation'] = True
+                else:
+                    context['string_validation'] = False
+            
+                return render(request, 'regularexpressions/index.html', context=context)
+            except Exception as e:
+                context['invalid_regex'] = True
+                context['error_message'] = str(e)
+                return render(request, 'regularexpressions/index.html', context=context)
 
     else:
         context['regexform'] = RegexForm()
@@ -28,8 +33,11 @@ def regex_page(request):
 
 def verify_regex(regex, string):
     print(f'\nVerifying REGEX: {regex} for string {string}\n')
-    valid = re.compile(regex)
-    if(valid.match(string)):
-        return True
-    else:
-        return False
+    try: # if re.compile works
+        valid = re.compile(regex)
+        if(valid.match(string)):
+            return True
+        else:
+            return False
+    except Exception as e:
+        raise e
--- PyFlap/static/js/sidebar.js
+++ PyFlap/static/js/sidebar.js
@@ -1,3 +1,46 @@
+
+// // ADDING NAVBAR ITEMS
+// const navBar = document.querySelectorAll(".nav_list");
+// console.log(navBar[0]);
+// // homepage item
+// const homePageItem = document.createElement('a');
+// homePageItem.href = "/";
+// homePageItem.className = "nav_link active";
+// homePageItem.innerHTML = "<i class='bx bx-grid-alt nav_icon'></i> <span class='nav_name'>Presentation</span>";
+// // participants page item
+// const participantsPageItem = document.createElement('a');
+// participantsPageItem.href = "#";
+// participantsPageItem.className = "nav_link";
+// participantsPageItem.innerHTML = "<i class='bx bx-user nav_icon'></i> <span class='nav_name'>Participants</span> ";
+// // Regex page item
+// const regexPageItem = document.createElement('a');
+// regexPageItem.href = "/regularexpressions/";
+// regexPageItem.className = "nav_link";
+// regexPageItem.innerHTML = "<i class='bx bx-message-square-detail nav_icon'></i> <span class='nav_name'>Regex Validator</span> ";
+// // Bookmark page item
+// const bookmarkPageItem = document.createElement('a');
+// bookmarkPageItem.href = "#";
+// bookmarkPageItem.className = "nav_link";
+// bookmarkPageItem.innerHTML = "<i class='bx bx-bookmark nav_icon'></i> <span class='nav_name'>Bookmark</span>";
+// // Files page item
+// const filesPageItem = document.createElement('a');
+// filesPageItem.href = "#";
+// filesPageItem.className = "nav_link";
+// filesPageItem.innerHTML = "<i class='bx bx-folder nav_icon'></i> <span class='nav_name'>Files</span> ";
+// // Stats page item
+// const statsPageItem = document.createElement('a');
+// statsPageItem.href = "#";
+// statsPageItem.className = "nav_link";
+// statsPageItem.innerHTML = "<i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class='nav_name'>Stats</span>";
+
+// //Appending all pages item to sidebar
+// navBar[0].appendChild(homePageItem);
+// navBar[0].appendChild(participantsPageItem);
+// navBar[0].appendChild(regexPageItem);
+// navBar[0].appendChild(bookmarkPageItem);
+// navBar[0].appendChild(filesPageItem);
+// navBar[0].appendChild(statsPageItem);
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
     const showNavbar = (toggleId, navId, bodyId, headerId) => {
@@ -24,15 +67,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
     showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
 
     /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link');
-    console.log(linkColor);
-    function colorLink() {
-        if (linkColor) {
-            linkColor.forEach(l => { l.classList.remove('active'); });
-            this.classList.add('active');
+    const linkList = document.querySelectorAll('.nav_link');
+
+    // function changeActiveLink() {
+    //     const activeLink = document.querySelectorAll('.active');
+    //     activeLink[0].classList.remove('active');
+    //     this.classList.add('active');
+    //     linkList.forEach(l => { console.log(l.outerHTML) });
+
+    // }
+    // linkList.forEach(l => { l.addEventListener('click', changeActiveLink); });
+
+
+    linkList.forEach(l => {
+        if (l.href === window.location.href) {
+            const activeLink = document.querySelectorAll('.active');
+            activeLink[0].classList.remove('active');
+            l.classList.add('active');
         }
-    }
-    linkColor.forEach(l => l.addEventListener('click', colorLink));
+    });
+
+    console.log("The URL of this page is: " + window.location.href);
 
     // Your code to run since DOM is loaded and ready
 });
+
