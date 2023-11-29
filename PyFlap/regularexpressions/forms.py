9541f42570063b05178f32363ff002de6b78a8d6
SeaSkyThe <marceloer2011@gmail.com>
2023-11-28 23:47:50 -0300

    changing some texts

--- PyFlap/regularexpressions/forms.py
+++ PyFlap/regularexpressions/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class RegexForm(forms.Form):
     regularexpression = forms.CharField(label='Your Regular Expression', max_length=1000)
-    regularexpression.widget = forms.TextInput(attrs={'title':'regularexpression', 'placeholder': 'Type your regular expression', 'class': 'round', 'id': 'regex',})
+    regularexpression.widget = forms.TextInput(attrs={'title':'regularexpression', 'placeholder': 'Digite sua expressão regular', 'class': 'round', 'id': 'regex',})
 
     teststring = forms.CharField(label='Your test string', max_length=100, empty_value='', required=False)
-    teststring.widget = forms.TextInput(attrs={'title':'teststring', 'placeholder': 'Type your string to test', 'class': 'round', 'id': 'test_string',})
+    teststring.widget = forms.TextInput(attrs={'title':'teststring', 'placeholder': 'Digite sua string de teste', 'class': 'round', 'id': 'test_string',})
     
     teststring2 = forms.CharField(label='Your test string', max_length=100, empty_value='', required=False)
-    teststring2.widget = forms.TextInput(attrs={'title':'teststring', 'placeholder': 'Type your string to test', 'class': 'round', 'id': 'test_string2',})
+    teststring2.widget = forms.TextInput(attrs={'title':'teststring', 'placeholder': 'Digite sua string de teste', 'class': 'round', 'id': 'test_string2',})
